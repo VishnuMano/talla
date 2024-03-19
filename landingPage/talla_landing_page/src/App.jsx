843bf0212362142aeac05ccc4a9ea710e28d692d
Vishnu Mano <vishnu.mano@gmail.com>
2024-03-19 13:32:46 -0400

    waitlist frontend

--- landingPage/talla_landing_page/src/App.jsx
+++ landingPage/talla_landing_page/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import styles from './style';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+    </div>
   )
 }
 
